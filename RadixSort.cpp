6bb113ecddbeab31b1e128ff73762eb0a0f511d0
YEJIRYOO <110809579+YEJIRYOO@users.noreply.github.com>
2023-10-02 15:37:28 +0900

    RadixSort_LinkedList

--- RadixSort.cpp
+++ RadixSort.cpp
@@ -1,85 +1,117 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<queue>
-
-#define MAX 100
+#include <iostream>
 using namespace std;
 
-int Max_Value;
-int Arr[MAX];
-bool Flag[10001];
-queue<int> Q[10];
+// 연결 리스트 노드 클래스 정의
+class Node {
+public:
+    int data;
+    Node* next;
 
-void Print()
-{
-    cout << "####################################################################################################################" << endl;
-    int Cnt = 0;
-    for (int i = 0; i < MAX; i++)
-    {
-        printf("%6d ", Arr[i]);
-        Cnt++;
-        if (Cnt == 20)
-        {
-            Cnt = 0;
-            cout << endl;
-        }
+    Node(int val) {
+        data = val;
+        next = nullptr;
     }
-    cout << "####################################################################################################################" << endl;
-    cout << endl;
-}
+};
 
-void Radix_Sort()
-{
-    int Radix = 1;
-    while (1)
-    {
-        if (Radix >= Max_Value) break;
-        Radix = Radix * 10;
+// 연결 리스트 클래스 정의
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
     }
-    for (int i = 1; i < Radix; i = i * 10)
-    {
-        for (int j = 0; j < MAX; j++)
-        {
-            int K;
-            if (Arr[j] < i) K = 0;
-            else K = (Arr[j] / i) % 10;
-            Q[K].push(Arr[j]);
-        }
 
-        int Idx = 0;
-        for (int j = 0; j < 10; j++)
-        {
-            while (Q[j].empty() == 0)
-            {
-                Arr[Idx] = Q[j].front();
-                Q[j].pop();
-                Idx++;
+    // 연결 리스트에 노드 추가
+    void append(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
             }
+            current->next = newNode;
+        }
+    }
+
+    // 연결 리스트 출력
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    // 연결 리스트 비우기
+    void clear() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+        head = nullptr;
+    }
+};
+
+// 각 자릿수 기준 정렬
+void countingSort(LinkedList& arr, int exp) {
+    LinkedList output[10];
+    Node* current = arr.head;
+
+    while (current != nullptr) {
+        int digit = (current->data / exp) % 10;
+        output[digit].append(current->data);
+        current = current->next;
+    }
+
+    arr.clear();
+
+    for (int i = 0; i < 10; i++) {
+        current = output[i].head;
+        while (current != nullptr) {
+            arr.append(current->data);
+            current = current->next;
         }
     }
 }
 
-int main(void)
-{
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < MAX; )
-    {
-        Arr[i] = (rand() % 10000) + 1;
-        if (Flag[Arr[i]] == false)
-        {
-            Flag[Arr[i]] = true;
-            if (Max_Value < Arr[i]) Max_Value = Arr[i];
+// 기수 정렬 함수
+void radixSort(LinkedList& arr) {
+    int maxVal = 0;
+    Node* current = arr.head;
 
-            i++;
-        }
+    // 최대값 찾기
+    while (current != nullptr) {
+        if (maxVal < current->data) maxVal = current->data;
+        current = current->next;
     }
 
-    cout << "[ Initialize Array State ] " << endl;
-    Print();
-    Radix_Sort();
-    cout << "[ After Sort Array State ] " << endl;
-    Print();
+    // 가장 큰 자릿수까지 반복적으로 정렬
+    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
+        countingSort(arr, exp);
+    }
+}
+
+int main(void) {
+    LinkedList arr;
+    arr.append(170);
+    arr.append(45);
+    arr.append(75);
+    arr.append(90);
+    arr.append(802);
+    arr.append(24);
+    arr.append(3000);
+
+    radixSort(arr);
+
+    // 결과 출력
+    arr.display();
 
     return 0;
 }
