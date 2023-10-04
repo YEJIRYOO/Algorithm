284633092144de73d0464aa895af8002fcc2a39e
YEJIRYOO <110809579+YEJIRYOO@users.noreply.github.com>
2023-10-04 23:42:15 +0900

    정렬 언어제한

--- boj2751.cpp
+++ boj2751.cpp
@@ -1,3 +1,6 @@
+//boj2751
+//문자열 오름차순
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -14,4 +17,35 @@ int main() {
     sort(a.begin(), a.end());
     for (int i = 0; i < num; i++)
         cout << a[i] << '\n';
+}
+
+*/
+
+//boj10989
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T;
+    cin >> T;
+
+    int arr[10001] = { 0 };
+
+    // 숫자 개수 카운트
+    for (int i = 0; i < T; i++) {
+        int a;
+        cin >> a;
+        arr[a] += 1;
+    }
+
+    // 각 숫자를 개수만큼 출력해주기
+    for (int i = 1; i <= 10000; i++)
+        for (int j = 0; j < arr[i]; j++)
+            cout << i << "\n";
 }
