7c2d25221d32f89bbecac41675c3043ef7f7d0a2
YEJIRYOO <110809579+YEJIRYOO@users.noreply.github.com>
2023-10-29 21:18:53 +0900

    1차

---
+++ select.cpp
@@ -0,0 +1,26 @@
+//평균 선형시간 선택 알고리즘
+/*
+#include<iostream>
+using namespace std;
+
+void partition(int A[], int p, int r) {
+
+}
+
+void buildHeap(int A[], int n) {
+
+}
+
+void heapify(int A[], int k, int n) {
+
+}
+
+void select(int A[], int p, int r, int i) {
+
+}
+
+int main(void) {
+
+	return 0;
+}
+*/
