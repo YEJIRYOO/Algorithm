2e914c3140abd404b0bed5c17470aba80da39716
YEJIRYOO <110809579+YEJIRYOO@users.noreply.github.com>
2023-10-30 20:03:39 +0900

    과제 4_2차

--- linearSelect.cpp
+++ linearSelect.cpp
@@ -1,201 +1,58 @@
-//1-1 알고리즘 5-1
-//평균 선형시간 선택 알고리즘
-/*
+//최악 경우 선형시간 선택 알고리즘
+
 #include<iostream>
-#include<ctime>
 using namespace std;
 
 void swap(int A[], int a, int b) {
-	int tmp = A[a];
-	A[a] = A[b];
-	A[b] = tmp;
+    int tmp = A[a];
+    A[a] = A[b];
+    A[b] = tmp;
 }
 
 int partition(int A[], int p, int r) {
-	int x = A[r];
-	int i = p - 1;
+    int x = A[r];
+    int i = p - 1;
 
-	for (int j = p; j <= r - 1; j++) {
-		if (A[j] < x) {
-			i++;
-			swap(A, i, j);
-		}
-	}
+    for (int j = p; j <= r - 1; j++) {
+        if (A[j] < x) {
+            i++;
+            swap(A, i, j);
+        }
+    }
 
-	swap(A, i + 1, r);
+    swap(A, i + 1, r);
 
-	return i + 1;
+    return i + 1;
 }
 
-int select(int A[], int p, int r, int i) {
-	if (p == r) return A[p];//원소 하나
+void heapify(int A[], int k, int n) {
+    int left = 2*k;
+    int right = 2 * k + 1;
+    int smaller = 0;
 
-	int q = partition(A, p, r);
-	int k = q - p + 1;
+    if (right <= n) {
+        smaller = A[left] < A[right] ? left : right;
+    }
+    else if (left <= n)smaller = left;
+    else return;
 
-    if (i == k) return A[q]; // 기준원소 == 찾는 원소
-    else if (i < k) return select(A, p, q - 1, i); // 왼쪽 그룹으로 범위 좁힘
-    else return select(A, q + 1, r, i - k); // 오른쪽 그룹으로 범위 좁힘
+    if (A[smaller] < A[k]) {
+        swap(A, k, smaller);
+        heapify(A, smaller, n);
+    }
+}
+
+void buildHeap(int A[], int n) {//배열A를 힙구조로 만든다
+	for (int i = n / 2; i >= 1; i--) {
+		heapify(A, i, n);
+	}
+}
+
+void linearSelect(int A[], int p, int r, int i) {
+
 }
 
 int main(void) {
-	time_t start, finish;
-	double duration;
-
-	start = time(NULL);
-
-	int A[100000];
-	srand((unsigned int)time(NULL));
-
-	for (int i = 0; i < 100000; i++) {
-		A[i] = rand()%10000;
-	}
-
-	for (int k = 1; k < 50001; k++) {
-		select(A, 0, 99999,k);
-	}
-
-	finish = time(NULL);
-
-	duration = (double)(finish - start);
-	cout << duration << "초" << endl;
 
 	return 0;
-}
-*/
-/*
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include<random>
-using namespace std;
-
-void swap(int A[], int a, int b) {
-    int tmp = A[a];
-    A[a] = A[b];
-    A[b] = tmp;
-}
-
-int partition(int A[], int p, int r) {
-    int x = A[r];
-    int i = p - 1;
-
-    for (int j = p; j <= r - 1; j++) {
-        if (A[j] < x) {
-            i++;
-            swap(A, i, j);
-        }
-    }
-
-    swap(A, i + 1, r);
-
-    return i + 1;
-}
-
-int select(int A[], int p, int r, int i) {
-    if (p == r) return A[p]; // 원소 하나
-
-    int q = partition(A, p, r);
-    int k = q - p + 1;
-
-    if (i == k) return A[q]; // 기준원소 == 찾는 원소
-    else if (i < k) return select(A, p, q - 1, i); // 왼쪽 그룹으로 범위 좁힘
-    else return select(A, q + 1, r, i - k); // 오른쪽 그룹으로 범위 좁힘
-}
-
-int main() {
-    int A[100000];
-    srand(static_cast<unsigned int>(time(NULL)));
-
-    for (int i = 0; i < 100000; i++) {
-        A[i] = rand() % 1000000; // 0에서 999999 사이의 랜덤한 값으로 배열 초기화
-    }
-
-    time_t start, finish;
-    double duration;
-
-    start = time(NULL);
-
-    int k = 50000; // 찾을 작은 값의 위치 (1부터 50,000까지)
-
-    int result = select(A, 0, 99999, k);
-
-    finish = time(NULL);
-
-    duration = difftime(finish, start);
-
-    cout << "1부터 " << k << "번째 작은 값: " << result << endl;
-    cout << "소요 시간: " << duration << "초" << endl;
-
-    return 0;
-}
-*/
-
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-using namespace std;
-
-void swap(int A[], int a, int b) {
-    int tmp = A[a];
-    A[a] = A[b];
-    A[b] = tmp;
-}
-
-int partition(int A[], int p, int r) {
-    int x = A[r];
-    int i = p - 1;
-
-    for (int j = p; j <= r - 1; j++) {
-        if (A[j] < x) {
-            i++;
-            swap(A, i, j);
-        }
-    }
-
-    swap(A, i + 1, r);
-
-    return i + 1;
-}
-
-int select(int A[], int p, int r, int i) {
-    while (p < r) {
-        int q = partition(A, p, r);
-        int k = q - p + 1;
-
-        if (i == k) return A[q];
-        else if (i < k) r = q - 1;
-        else {
-            p = q + 1;
-            i -= k;
-        }
-    }
-
-    return A[p];
-}
-
-int main() {
-    int A[100000];
-    srand(static_cast<unsigned int>(time(NULL)));
-
-    for (int i = 0; i < 100000; i++) {
-        A[i] = rand() % 1000000;
-    }
-
-    time_t start, finish;
-    double duration;
-
-    start = time(NULL);
-
-    for (int i = 1; i <= 50000; i++) {
-        int result = select(A, 0, 99999, i);
-    }
-
-    finish = time(NULL);
-
-    duration = difftime(finish, start);
-
-    cout << "소요 시간: " << duration << "초" << endl;
-
-    return 0;
 }
--- select.cpp
+++ select.cpp
@@ -1,26 +1,138 @@
+
+//1-1 알고리즘 5-1
 //평균 선형시간 선택 알고리즘
 /*
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-void partition(int A[], int p, int r) {
-
+void swap(int A[], int a, int b) {
+    int tmp = A[a];
+    A[a] = A[b];
+    A[b] = tmp;
 }
 
-void buildHeap(int A[], int n) {
+int partition(int A[], int p, int r) {
+    int x = A[r];
+    int i = p - 1;
 
+    for (int j = p; j <= r - 1; j++) {
+        if (A[j] < x) {
+            i++;
+            swap(A, i, j);
+        }
+    }
+
+    swap(A, i + 1, r);
+
+    return i + 1;
 }
 
-void heapify(int A[], int k, int n) {
+int select(int A[], int p, int r, int i) {
+    if (p == r) return A[p];//원소 하나
 
-}
+    int q = partition(A, p, r);
+    int k = q - p + 1;
 
-void select(int A[], int p, int r, int i) {
-
+    if (i == k) return A[q]; // 기준원소 == 찾는 원소
+    else if (i < k) return select(A, p, q - 1, i); // 왼쪽 그룹으로 범위 좁힘
+    else return select(A, q + 1, r, i - k); // 오른쪽 그룹으로 범위 좁힘
 }
 
 int main(void) {
+    time_t start, finish;
+    double duration;
 
-	return 0;
+    start = time(NULL);
+
+    int A[100000];
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < 100000; i++) {
+        A[i] = rand()%10000;
+    }
+
+    for (int k = 1; k < 50001; k++) {
+        select(A, 0, 99999,k);
+    }
+
+    finish = time(NULL);
+
+    duration = (double)(finish - start);
+    cout << duration << "초" << endl;
+
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include<random>
+using namespace std;
+
+void swap(int A[], int a, int b) {
+    int tmp = A[a];
+    A[a] = A[b];
+    A[b] = tmp;
+}
+
+int partition(int A[], int p, int r) {
+    int x = A[r];
+    int i = p - 1;
+
+    for (int j = p; j <= r - 1; j++) {
+        if (A[j] < x) {
+            i++;
+            swap(A, i, j);
+        }
+    }
+
+    swap(A, i + 1, r);
+
+    return i + 1;
+}
+
+int select(int A[], int p, int r, int i) {
+    if (p == r) return A[p]; // 원소 하나
+
+    int q = partition(A, p, r);
+    int k = q - p + 1;
+
+    if (i == k) return A[q]; // 기준원소 == 찾는 원소
+    else if (i < k) return select(A, p, q - 1, i); // 왼쪽 그룹으로 범위 좁힘
+    else return select(A, q + 1, r, i - k); // 오른쪽 그룹으로 범위 좁힘
+}
+
+int main() {
+    int A[100000];
+    srand(static_cast<unsigned int>(time(NULL)));
+
+    for (int i = 0; i < 100000; i++) {
+        A[i] = rand() % 100000; // 0에서 999999 사이의 랜덤한 값으로 배열 초기화
+    }
+
+    time_t start, finish;
+    double duration;
+
+    start = time(NULL);
+
+    for (int j = 0; j < 50000; j++) select(A, 0, 99999, j);
+
+    finish = time(NULL);
+
+    duration = difftime(finish, start);
+
+    cout << "1부터 " << 50000 << "번째 작은 값: " << endl;
+    cout << "소요 시간: " << duration << "초" << endl;
+
+    return 0;
 }
 */
